3b86ee833b36e40396f3f4b21e301f5450e60aec
Frank Ayars <fayars@brandify.com>
2017-02-15 12:36:51 -0800

    update index.js

--- location/index.js
+++ location/index.js
@@ -1,22 +1,8 @@
-var location = require('@brandify/location');
-//require('@google/cloud-debug').start();
+var location = require('./node-library/location');
 
 exports.getLocationsHTTP = function getLocationsHTTP(req, res) 
 {
   res.setHeader('Content-Type', 'application/json');
-  var json = location.getLocationsByGeoCode(77.0, 88.0);
+  var json = location.getLocation("myId");
   res.status(200).send(json).end();
-};
-
-exports.getLocationsMQ = function getLocationsMQ(event, callback) {
-  
-  // The Cloud Pub/Sub Message object.
-  const pubsubMessage = event.data;
-
-  // We're just going to log the message to prove that
-  // it worked.
-  console.log(Buffer.from(pubsubMessage.data, 'base64').toString());
-
-    var json = location.getLocationsByGeoCode(77.0, 88.0);
-    callback(null, json);
 };
