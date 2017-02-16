94bc046b4d8b343cb320b68058958266c1a294bb
Frank Ayars <fayars@brandify.com>
2017-02-16 05:37:36 -0800

    updated index.js

--- location/index.js
+++ location/index.js
@@ -1,8 +1,7 @@
 var location = require('node-library/location/location');
 
-exports.getLocation = function getLocation(req, res) 
+exports.getLocation = function getLocation(event, callback) 
 {
-  res.setHeader('Content-Type', 'application/json');
   var json = location.getLocation("myId");
-  res.status(200).send(json).end();
+  callback(json);
 };
